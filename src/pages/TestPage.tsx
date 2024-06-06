2876f1ecc817d7b03b9a7b192f0c4945633e13f5
Nicholas Irvin Suhendi <nicholasirvin123@gmail.com>
2024-06-06 13:40:09 +0700

    feat: Test Page

---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/Sidebar.tsx";
+
+function TestPage() {
+    return (
+        <>
+            <div className={"w-screen h-screen bg-primary"}>
+                <Sidebar/>
+            </div>
+        </>
+    )
+}
+
+export default TestPage
