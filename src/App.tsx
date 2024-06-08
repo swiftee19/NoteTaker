7de0315146c74f0d8255f5143a507c827b1b7304
Nicholas Irvin Suhendi <nicholasirvin123@gmail.com>
2024-06-08 08:59:38 +0700

    feat: hashroute

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage.tsx";
 import SigninPage from "./pages/SigninPage.tsx";
 import {AuthProvider} from "./auth/AuthProvider.tsx";
@@ -9,20 +9,22 @@ function App() {
     return (
         <>
             <AuthProvider>
-                <Routes>
-                    <Route path={'/'} element={
-                        <LoginPage/>
-                    }/>
-                    <Route path={'signin'} element={
-                        <SigninPage/>
-                    }/>
-                    <Route path={'home/:noteId'} element={
-                        <PrivateRoute path="/home" element={<HomePage/>}/>
-                    }/>
-                    <Route path={'home'} element={
-                        <PrivateRoute path="/home" element={<HomePage/>}/>
-                    }/>
-                </Routes>
+                <HashRouter>
+                    <Routes>
+                        <Route path={'/'} element={
+                            <LoginPage/>
+                        }/>
+                        <Route path={'signin'} element={
+                            <SigninPage/>
+                        }/>
+                        <Route path={'home/:noteId'} element={
+                            <PrivateRoute path="/home" element={<HomePage/>}/>
+                        }/>
+                        <Route path={'home'} element={
+                            <PrivateRoute path="/home" element={<HomePage/>}/>
+                        }/>
+                    </Routes>
+                </HashRouter>
             </AuthProvider>
         </>
     )
